20352bea7c0d2ec7193319d69a6e8aa57732fbc9
linyichie <linyichie@gmail.com>
2020-04-15 18:08:15 +0800

    自定义导入资源设置

---
+++ Client/Assets/Editor/AssetImport/CustomAssetImport.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class CustomAssetImport : AssetPostprocessor {
+
+    public const string Platform_Standalone = "Standalone";
+    public const string Platform_iPhone = "iPhone";
+    public const string Platform_Android = "Android";
+
+    private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
+        string[] movedFromAssetPaths) {
+        for (int i = 0; i < importedAssets.Length; i++) {
+            OnPoseprocessWithExtension(importedAssets[i]);
+        }
+    }
+
+    private void OnPostprocessAnimation(GameObject root, AnimationClip clip) {
+        throw new NotImplementedException();
+    }
+
+    private void OnPostprocessAudio(AudioClip arg) {
+        throw new NotImplementedException();
+    }
+
+    private void OnPostprocessCubemap(Cubemap texture) {
+        throw new NotImplementedException();
+    }
+
+    private void OnPostprocessMaterial(Material material) {
+        throw new NotImplementedException();
+    }
+
+    private void OnPostprocessModel(GameObject g) {
+        throw new NotImplementedException();
+    }
+
+    private void OnPostprocessSprites(Texture2D texture, Sprite[] sprites) {
+        throw new NotImplementedException();
+    }
+
+    private void OnPostprocessTexture(Texture2D texture) {
+        throw new NotImplementedException();
+    }
+
+    private void OnPostprocessMeshHierarchy(GameObject root) {
+        throw new NotImplementedException();
+    }
+
+    private void OnPostprocessSpeedTree(GameObject arg) {
+        throw new NotImplementedException();
+    }
+
+    private void OnPostprocessAssetbundleNameChanged(string assetPath, string previousAssetBundleName,
+        string newAssetBundleName) {
+        throw new NotImplementedException();
+    }
+
+    private void OnPostprocessGameObjectWithUserProperties(GameObject go, string[] propNames, object[] values) {
+        throw new NotImplementedException();
+    }
+
+    private void
+        OnPostprocessGameObjectWithAnimatedUserProperties(GameObject gameObject, EditorCurveBinding[] bindings) {
+        throw new NotImplementedException();
+    }
+
+    private static void OnPoseprocessWithExtension(string assetName) {
+        if (assetName.EndsWith(".spriteatlas")) {
+            SpriteAtlasAssetImporter.OnPostprocessSpriteAtlas(assetName);
+        }
+    }
+}
---
+++ Client/Assets/Editor/AssetImport/SpriteAtlasAssetImporter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.U2D;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public static class SpriteAtlasAssetImporter {
+    private static SpriteAtlas spriteAtlas;
+
+    public static void OnPostprocessSpriteAtlas(string assetName) {
+        spriteAtlas = AssetDatabase.LoadAssetAtPath<SpriteAtlas>(assetName);
+        SetIncludeInBuild();
+        SetPackingSettings();
+        SetPlatformSettings();
+        EditorUtility.SetDirty(spriteAtlas);
+    }
+
+    static void SetIncludeInBuild() {
+        spriteAtlas.SetIncludeInBuild(true);
+    }
+
+    static void SetPackingSettings() {
+        var packSettings = spriteAtlas.GetPackingSettings();
+        spriteAtlas.SetPackingSettings(packSettings);
+    }
+
+    static void SetTextureSettings() {
+        var textureSettings = spriteAtlas.GetTextureSettings();
+        spriteAtlas.SetTextureSettings(textureSettings);
+    }
+
+    static void SetPlatformSettings() {
+        //-- Standalone
+        var platformSettings = spriteAtlas.GetPlatformSettings(CustomAssetImport.Platform_Standalone);
+        spriteAtlas.SetPlatformSettings(platformSettings);
+
+        //-- iPhone
+        platformSettings = spriteAtlas.GetPlatformSettings(CustomAssetImport.Platform_iPhone);
+        spriteAtlas.SetPlatformSettings(platformSettings);
+
+        //-- Android
+        platformSettings = spriteAtlas.GetPlatformSettings(CustomAssetImport.Platform_Android);
+        platformSettings.overridden = true;
+        spriteAtlas.SetPlatformSettings(platformSettings);
+    }
+}
