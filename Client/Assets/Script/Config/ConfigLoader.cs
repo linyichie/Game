3a1ab7db53090a6c69c7c47347d48b5867661e95
linyichie <linyichie@gmail.com>
2020-04-19 12:01:00 +0800

    提交配置解析

---
+++ Client/Assets/Script/Config/ConfigLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ConfigLoader {
+    public static void Initialize() {
+        LoadConfig("Geo", s => { GeoConfig.Parse(true, s, () => { Debug.Log(GeoConfig.Get(11).polygon); }); });
+    }
+
+    static void LoadConfig(string fileName, Action<string> callback) {
+        AddressableSystem.LoadAsset<TextAsset>(StringUtility.Contact("Txt/", fileName),
+            asset => { callback?.Invoke(asset.text); });
+    }
+}
---
+++ Client/Assets/Script/Examples/Config/TestConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEngine;
+
+public class TestConfig : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        ConfigLoader.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
